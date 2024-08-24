cdcc35c8448b54df988d59ff3630867e6f493cfa
Mike Root <michael.root@veteransunited.com>
2024-08-24 14:40:17 -0500

    Fix game controller

--- apis/Games.Minesweeper/Games.Minesweeper.API/Controllers/GameController.cs
+++ apis/Games.Minesweeper/Games.Minesweeper.API/Controllers/GameController.cs
@@ -1,1 +1,43 @@
-﻿
+using Microsoft.AspNetCore.Mvc;
+using Games.Minesweeper.Domain.Models;
+
+namespace Games.Minesweeper.API.Controllers
+{
+  /// <summary>
+  /// Game Controller
+  /// </summary>
+  [ApiController]
+  [Route("v1/[controller]")]
+  public class GameController : Controller
+  {
+    /// <summary>
+    /// Get starts a new game
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Block>>> GetNewGame()
+    {
+      return Ok(new List<Block>());
+    }
+
+    /// <summary>
+    /// Put checks a user's click to see if they've selected a mine
+    /// </summary>
+    /// <returns></returns>
+    [HttpPut]
+    public async Task<IActionResult> CheckClickedBlock()
+    {
+      return Ok(new List<Block>());
+    }
+
+    /// <summary>
+    /// Post saves the current game
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult> SaveGame()
+    {
+      return Ok(new List<Block>());
+    }
+  }
+}
